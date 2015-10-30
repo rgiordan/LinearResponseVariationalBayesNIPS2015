680ff19df0287d2cdf120398931eba4315917644
Ryan Giordano - SCF <rgiordan@gmail.com>
2015-10-29 22:12:05 -0700

    Typos

--- variational_normal_mixture/simulate_mvn_scaling.sh
+++ variational_normal_mixture/simulate_mvn_scaling.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# qsub -pe smp 4 simulate_mvn_scaling.sh
-export OMP_NUM_THREADS=1
-R CMD BATCH --args --cores=4 simulate_multivariate_normal_mixture_scaling.R simulate_mvn_scaling_smaller_cluster.Rout
+
+SH_FILE=/tmp/simulate_mvn_scaling.sh
+
+echo export OMP_NUM_THREADS=1 > $SH_FILE
+echo R CMD BATCH --args --cores=4 simulate_multivariate_normal_mixture_scaling.R simulate_mvn_scaling_smaller_cluster.Rout >> $SH_FILE
+chmod 700 $SH_FILE
+
+qsub -pe smp 4 $SH_FILE
